040e1484b9dada6a4211a1c1d1a044f50b38f051
killkelleyr <Robert.KillKelley@alertinnovation.com>
2020-06-17 20:01:29 -0400

    updated in-code documentation and set a default window size

--- stock_sim/cli/stockSim.py
+++ stock_sim/cli/stockSim.py
@@ -1,17 +1,20 @@
 import os
 import textwrap
-
+os.system('mode con: cols=100 lines=40')
 class cli():
         
     def printLogo(self):
-        logo = '''|                                                            |
-|       _____ _             _     _____ _____ __  __         |
-|      / ____| |           | |   / ____|_   _|  \/  |        |
-|     | (___ | |_ ___   ___| | _| (___   | | | \  / |        |
-|      \___ \| __/ _ \ / __| |/ /\___ \  | | | |\/| |        |
-|      ____) | || (_) | (__|   < ____) |_| |_| |  | |        |
-|     |_____/ \__\___/ \___|_|\_\_____/|_____|_|  |_|        |
-|                                                            |'''
+        '''
+        When called, this method outputs the logo surrounded in a border
+        '''
+        logo = '''|                                                                                                  |
+|                          _____ _             _     _____ _____ __  __                            |
+|                         / ____| |           | |   / ____|_   _|  \/  |                           |
+|                        | (___ | |_ ___   ___| | _| (___   | | | \  / |                           |
+|                         \___ \| __/ _ \ / __| |/ /\___ \  | | | |\/| |                           |
+|                         ____) | || (_) | (__|   < ____) |_| |_| |  | |                           |
+|                        |_____/ \__\___/ \___|_|\_\_____/|_____|_|  |_|                           |
+|                                                                                                  |'''
         self.headerFooter()
         print(logo)
         self.headerFooter()
@@ -25,12 +28,26 @@ class cli():
             os.system('clear')
     
     def headerFooter(self):
-        print("--------------------------------------------------------------")
+        '''
+        When called, this method creates the header/footer to break up a section
+        '''
+        print("----------------------------------------------------------------------------------------------------")
         
     def printOptions(self, options):
         """
-        prints whatever options are sent
+        When this method is called and supplied with a dictionary of options,
+            the available options will be displayed and it will prompt the user
+            to -- "Please Enter an option:"
+            
+        Arguments:
+            options(dict) - dictionary where key is the option id and the value
+                is the text that will be shown
+        
+        Returns:
+            chosenOption - after a user successfully choses an option, this 
+                method will return the chosen id
         """
+        
         self.headerFooter()
         for x in options:
             print(textwrap.fill("Option {}: {}".format(x, options[x]) , width=62))
@@ -40,18 +57,31 @@ class cli():
             chosenOption = input("Please enter an option: ")
             if chosenOption in options.keys():
                 return chosenOption
-    
-    def validateOption():
-        """
-        Use this to validate an option chosen from the printOptions function.
-            Will check than the option chose was a vaild choice
-        """
-        #TODO: ADD THIS IN
             
     def printInput(self, message):
+        '''
+        When called, this method allows a user to input text for use in a 
+            custom place
+            
+        Arguments:
+            message(string) - string used for prompting the user what to do next
+        
+        Returns:
+            string of what the user entered in this step
+        '''
         return(input(textwrap.fill(message, width = 62) + " "))
     
     def printLine(self, message):
+        '''
+        When called, this method allows to print messages to the screen and 
+            should be used to display information to the user.
+            
+        Arguments:
+            message(string) - string used for displaying text to the user
+        
+        Returns:
+            None - prints to screen only
+        '''
         print(textwrap.fill(message, width = 62))
 
 class normalWorkflow():
@@ -62,10 +92,7 @@ class normalWorkflow():
     def workflow(self):
         options={'1':'Test Option number 1',
                  '2': 'Test Option number 2 where we see how long we can run this sentence on for'}
-        optionChosen=None
-        print(options.keys())
-        for x in options.keys():
-            print(x)
+
         
         self.display.printLogo()
         optionChosen = self.display.printOptions(options)
