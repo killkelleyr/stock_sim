65a5f08f79c19244b9dc0dbc1d6c4b6dff4dfbd1
killkelleyr <Robert.KillKelley@alertinnovation.com>
2020-06-27 17:52:47 -0400

    Added buy/sell workflow and made minor changes to the buy_sell module

--- stock_sim/modules/buy_sell.py
+++ stock_sim/modules/buy_sell.py
@@ -1,6 +1,5 @@
 from dbWriter import dbWriter
 from logger import logger
-from stock import stock
 import requests
 import os
 import datetime
@@ -10,28 +9,30 @@ import time
 buySellLogger = logger()
 
 class buy_sell(object):
-    def __init__(self,ticker,volume,buysell,user_id):
+    def __init__(self,stock,volume,buysell,userName):
         sim = True
         self.timer = (random.randint(100,400)/1000)
-        purchese = False
-        if buysell == 'buy':
-            buysell = 1
-        elif buy_sell == 'sell':
-            buy_sell = 0
 
         if sim == False:
+            #TODO: Utilize td's trading API to create an order
+            '''
             try:
                 APIKEY = os.environ['APIKEY']
-                TD_transaction = requests.get('https://api.tdameritrade.com/v1/marketdata/{}/quotes?apikey={}'.format(ticker,APIKEY))
+                TD_transaction = requests.get('https://api.tdameritrade.com/v1/marketdata/{}/quotes?apikey={}'.format(stock.symbol,APIKEY))
                 "(logic for determination of a good perches whenth trew)"
                 buySellLogger.info("{} of {} was purchesed").format()
                 #set athurisation time
             except:
                 print("API purches faild")
-
+            '''
+            pass
         else:
             time.sleep(self.timer)
-            stock_info=stock(ticker)
+            stock_info=stock.lookupTicker(stock.symbol)
             transaction_time=datetime.datetime.now()
 
-        dbWriter.writetransactionhistory(user_id,ticker,stock_info.askPrice,volume,buysell,transaction_time)
+        dbWriter.writetransactionhistory(userName.userName,stock_info.symbol,stock_info.askPrice,volume,buysell,transaction_time)
+        if buysell == 1:
+            userName.portfolio.addstock(stock_info, volume, transaction_time)
+        elif buysell == 0:
+            userName.portfolio.sellstock(stock_info, volume, transaction_time)
