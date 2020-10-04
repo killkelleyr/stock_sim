2248cce2a602d32c96aaeefa14841a0dbafb0985
Robert KillKelley <robertkillkelley@gmail.com>
2020-10-04 11:39:50 -0400

    issue-12: added requirements file

--- StockSim/StockSim/settings.py
+++ StockSim/StockSim/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_cron',
+    'simple_history',
 ]
 
 MIDDLEWARE = [
@@ -48,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'simple_history.middleware.HistoryRequestMiddleware',
 ]
 
 ROOT_URLCONF = 'StockSim.urls'
