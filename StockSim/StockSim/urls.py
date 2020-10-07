a0932774600e37a41c8e86f5b1d67461ca936d96
Robert KillKelley <robertkillkelley@gmail.com>
2020-10-06 20:55:36 -0400

    Revert "issue-12:"
    This reverts commit 0e0669fb673503d0219527b75c011a686c51e4ea.

--- StockSim/StockSim/urls.py
+++ StockSim/StockSim/urls.py
@@ -14,11 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from django.conf.urls import url
+from django.urls import path
 
 urlpatterns = [
-    path('accounts/', include('accounts.urls')),
-    url('', include('portfolio.urls'), name='home'),
     path('admin/', admin.site.urls),
 ]
