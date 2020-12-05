a79f842451054df6f624dcbe1c837ab3bef0a2b8
Robert KillKelley <robertkillkelley@gmail.com>
2020-12-04 20:18:20 -0500

    issue-30: adding inital portfolio screen as well as changes for env variables

--- StockSim/trader/views.py
+++ StockSim/trader/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views import generic
+from django.views.generic import ListView
+import os, base64
+from Robinhood import Robinhood
 
-# Create your views here.
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from accounts.decorators import allowed_users
+
+@method_decorator(login_required, name='dispatch')
+class MarketView(generic.ListView):
+    template_name='trader/lookup.html'
+    queryset = ''
+
+    def get(self, request):
+        my_trader = Robinhood()
+        my_trader.login(username=os.environ['rhu'], password=base64.b64decode(os.environ['rhp']).decode('utf-8'), qr_code=os.environ['rhq'])
+        my_trader.logout()
+        context={}
+        return render(request, self.template_name, context)
+
+@method_decorator(login_required, name='dispatch')
+class TradeView(generic.ListView):
+    template_name='trader/trade.html'
+    queryset = ''
